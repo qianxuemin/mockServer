40d6ac029dffb8661223abc2a4e6b52fe88f8123
qianxuemin001 <qianxuemin001@ke.com>
2022-06-23 20:55:35 +0800

    feat: 自动获取端口

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 #!/usr/bin/env node
 
 const program = require('commander')
+const portfinder = require('portfinder')
 const pkg = require('./package.json')
 const checkVersion = require('./check-version')
 
-checkVersion(() => {
+checkVersion(async () => {
   const mockServer = require('./mock-server')
+  let defaultPort 
+
+  try {
+    defaultPort = await portfinder.getPortPromise()
+  } catch (error) {
+    defaultPort = 8080
+  }
+  
   program
     .version(pkg.version)
     .usage('[options]')
     .option('-p, --port [port]', '指定mock服务端口')
     .option('-d, --delay [delay]', '指定mock服务端时延，number or random')
     .parse(process.argv)
-
-  mockServer(program.port || 8080, program.delay || 0)
+  
+  mockServer(program.port || defaultPort, program.delay || 0)
 })
